51340f6f85a267d5291378d223b2ff7030f47317
XxStreAmeRbgXx <streamer_bg@abv.bg>
2020-03-23 17:22:10 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WalkingMan1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Man = new System.Windows.Forms.PictureBox();
+            this.action = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Man)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Man
+            // 
+            this.Man.Image = global::WalkingMan1._0.Properties.Resources._1;
+            this.Man.Location = new System.Drawing.Point(12, 291);
+            this.Man.Name = "Man";
+            this.Man.Size = new System.Drawing.Size(100, 100);
+            this.Man.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Man.TabIndex = 0;
+            this.Man.TabStop = false;
+            // 
+            // action
+            // 
+            this.action.Tick += new System.EventHandler(this.action_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Man);
+            this.Name = "Form1";
+            this.Text = "WalkingMan1.0";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Man)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Man;
+        private System.Windows.Forms.Timer action;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WalkingMan1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dist = 1;
+        int spd = 5;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                action.Start();
+                if (spd > 0)
+                {
+                    spd = -spd;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                action.Start();
+                if (spd < 0)
+                {
+                    spd = -spd;
+                }
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                action.Stop();
+                Man.Image = Properties.Resources._1;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void action_Tick(object sender, EventArgs e)
+        {
+            dist++;
+            Man.Left += spd;
+            if (dist == 1)
+            {
+                Man.Image = Properties.Resources._1;
+            }
+            if (dist == 2)
+            {
+                Man.Image = Properties.Resources._2;
+            }
+            if (dist == 3)
+            {
+                Man.Image = Properties.Resources._4;
+            }
+            if (dist == 4)
+            {
+                Man.Image = Properties.Resources._3;
+            }
+            if (dist == 5)
+            {
+                dist = 1;
+            }
+            if (Man.Left >= ClientSize.Width)
+            {
+                Man.Left = 2;
+            }
+            if (Man.Left <= 0)
+            {
+                Man.Left = ClientSize.Width - 2;
+            }
+            if (spd < 0)
+            {
+
+                Man.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            }
+        }
+    }
+}
